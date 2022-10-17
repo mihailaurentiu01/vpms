402d17fa4532020898d2587582e583b4e948965f
Mihai Laurentiu Iordache <adiordache61@gmail.com>
2022-10-17 21:45:02 +0200

    - Added react-router-dom - Added parking logo - Added login form (not functional yet) - Added main page - Added styling

---
+++ src/components/LoginForm/index.tsx
@@ -0,0 +1,71 @@
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Grid from '@mui/material/Grid';
+import { Button } from '@mui/material';
+import { useTranslation } from 'react-i18next';
+import React from 'react';
+
+function LoginForm() {
+  const { t } = useTranslation();
+
+  const onSubmitHandler = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    console.log('submited');
+  };
+
+  return (
+    <>
+      <Container maxWidth='md'>
+        <Box
+          sx={{
+            bgcolor: '#cfe8fc',
+            flexGrow: 1,
+          }}
+        >
+          <Typography align='center' variant='h2'>
+            {t('login')}
+          </Typography>
+
+          <Box
+            component='form'
+            sx={{}}
+            noValidate
+            autoComplete='off'
+            onSubmit={onSubmitHandler}
+          >
+            <Grid container spacing={2} justifyContent='center' sx={{ p: 1 }}>
+              <Grid item xs={12}>
+                <TextField
+                  sx={{ width: { xs: 1, md: 1 } }}
+                  type='email'
+                  label={t('signupForm.email')}
+                  variant='outlined'
+                  required
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  sx={{ width: { xs: 1, md: 1 } }}
+                  type='password'
+                  label={t('signupForm.password')}
+                  variant='outlined'
+                  required
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <Button type='submit' sx={{ width: 1 }} variant='contained'>
+                  {t('login')}
+                </Button>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+      </Container>
+    </>
+  );
+}
+
+export default LoginForm;
--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,13 +1,27 @@
 import CssBaseline from '@mui/material/CssBaseline';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
+import parkingLogo from '../../assets/img/parking.png';
+import styles from './main.module.css';
 
 const Main = () => {
   return (
     <>
       <CssBaseline />
-      <Container maxWidth='sm' fixed>
-        <Box sx={{ bgcolor: '#cfe8fc' }} />
+      <Container maxWidth={false}>
+        <Grid container spacing={2} justifyContent='center' sx={{ p: 1 }}>
+          <Grid item>
+            <h1>VEHICLE PARKING MANAGEMENT SYSTEM</h1>
+
+            <img
+              className={styles.center}
+              src={parkingLogo}
+              alt='Parking logo'
+              width='250'
+            />
+          </Grid>
+        </Grid>
       </Container>
     </>
   );
